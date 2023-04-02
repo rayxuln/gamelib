e7a079a791170c7dcb7bb98798e5c739af1ac713
Raiix <imbosslx@sina.com>
2023-04-02 16:59:20 +0800

    update chat app

--- chat_app/index.service.worker.js
+++ chat_app/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1680420776|3484682827";
+const CACHE_VERSION = "1680425784|295461968";
 const CACHE_PREFIX = "Chat App-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
